ef21b421b6a0231eaeb5435e0166a52c9a5f8663
felixschulz385 <felix@zoelch-schulz.de>
2022-06-13 18:27:18 +0200

    Added most basic functions for sqlite

--- JDBI/JDBI.py
+++ JDBI/JDBI.py
@@ -1,4 +1,4 @@
-def dbConnect(drv, dbname, host, user, password):
+def dbConnect(drv, host, user=None, password=None):
     """
     Connect to a SQL database
     Args:
@@ -11,23 +11,54 @@ def dbConnect(drv, dbname, host, user, password):
         A Connection object
     """
     
-    def mysql_connect(dbname, host, user, password):
+    def mysql_connect(host, user, password):
         import mysql.connector
-        mysql.connector.connect(host=host,
-                                user=user,
-                                password=password)
+        return mysql.connector.connect(host=host,
+                                       user=user,
+                                       password=password)
     
-    def sqlite_connect(dbname, host, user, password):
+    def sqlite_connect(database):
         import sqlite3
-        con = sqlite3.connect(host = host,
-                              user=user,
-                              password=password)
+        return sqlite3.connect(database = host)
     
     match drv:
         case "mysql":
-            mysql_connect(host=host, user=user, password=password)
+            return mysql_connect(host=host, user=user, password=password)
         case "sqlite":
-            sqlite_connect(host=host, user=user, password=password)
+            return sqlite_connect(database=host)
 
-def dbListTables():
-    ""
+def dbListTables(con):
+    """
+    Lists all tables of a database
+    Args:
+        con (Connection): the connection to the database
+    Returns:
+        An array containing the names of the tables in the database
+    """
+    
+    def sqlite_list_tables(con):
+        from itertools import chain
+        cursor = con.cursor()
+        cursor.execute("SELECT name FROM sqlite_master where type='table'")
+        return list(chain.from_iterable(cursor.fetchall()))
+    
+    match str(type(con)):
+        case "<class 'sqlite3.Connection'>":
+            return sqlite_list_tables(con)
+        
+def dbDisconnect(con):
+    """
+    Closes the connection
+    Args:
+        con (Connection): the connection to the database
+    Returns:
+        None
+    """
+    
+    def sqlite_disconnect(con):
+        con.close()
+    
+    match str(type(con)):
+        case "<class 'sqlite3.Connection'>":
+            return sqlite_disconnect(con)
+    
